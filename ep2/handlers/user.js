dd0858ff161bd22b53e13a1be4cc8a7040c9740d
Andrey Kostin <losogudok@yandex.ru>
2015-07-04 23:31:24 +0300

    User handler

--- ep2/handlers/user.js
+++ ep2/handlers/user.js
@@ -1,3 +1,42 @@
-/**
- * Created by andrey on 04.07.15.
- */
+var BaseModel = require('../models/base');
+
+function readBody(req) {
+	return new Promise(function(resolve, reject){
+		var body = '';
+
+	  	req.setEncoding('utf8');
+
+	  	req.on('data', function (chunk) {
+	    	body += chunk;
+	  	});
+
+	  	req.on('error', function(err){
+	  		reject(err);
+	  	});
+
+	  	req.on('close', function(){
+
+	  	});
+
+	  	req.on('end', function(){
+	  		resolve(body);
+	  	});
+	});
+}
+
+module.exports = function(req, res) {
+	var json;
+	
+
+	switch (req.method) {
+		case 'POST':
+		BaseModel.create('users', json);
+		break;
+		case 'PUT':
+		break;
+		case 'DELETE':
+		break;
+		case 'GET';
+		break;
+	}
+}
